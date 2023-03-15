9da8d8a73c4921c0d63a99ca23d478c8301d92e8
Jackson Freitas <jacksonzitap.mc@gmail.com>
2023-03-15 07:33:23 -0300

    doing menu-mobile

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+const btnMobile = document.querySelector('.btn-mobile');
+
+btnMobile.addEventListener('click', toggleMenu)
+
+function toggleMenu(){
+    const nav = document.querySelector('.nav');
+    nav.classList.toggle('active');
+    console.log('clique')
+}
+
