daaceb66cc8397d5a316f54cd6190df5bc1f26af
Jackson Freitas <jacksonzitap.mc@gmail.com>
2023-03-16 00:55:54 -0300

    add mobile menu and adjust mobile size

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,21 @@
-const btnMobile = document.querySelector('.btn-mobile');
+const btnOpenMenu = document.querySelector('.open-menu-icon');
+const btnCloseMenu = document.querySelector('.close-menu-icon'); 
+// const nav = document.querySelector('.nav');
+const menu = document.querySelector('.menu');
 
-btnMobile.addEventListener('click', toggleMenu)
+btnOpenMenu.addEventListener('click', openMenu);
+btnCloseMenu.addEventListener('click', closeMenu);
 
-function toggleMenu(){
-    const nav = document.querySelector('.nav');
-    nav.classList.toggle('active');
-    console.log('clique')
+function openMenu(){
+    btnOpenMenu.classList.add('hide');
+    btnCloseMenu.classList.remove('hide');
+    // nav.classList.add('active');
+    menu.classList.add('active');
 }
 
+function closeMenu(){
+    btnOpenMenu.classList.remove('hide');
+    btnCloseMenu.classList.add('hide');
+    // nav.classList.remove('active');
+    menu.classList.remove('active');
+}
